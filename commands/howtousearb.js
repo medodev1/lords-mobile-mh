c93a9e8b481173769c5d7a329e966c98adfcdb02
medodev1 <64170763+medodev1@users.noreply.github.com>
2020-07-07 16:50:23 +0200

    Update howtousearb.js

--- commands/howtousearb.js
+++ commands/howtousearb.js
@@ -3,12 +3,12 @@ const botconfig = require("../botsettings.json")
 const attachment = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 const embed = new Discord.MessageEmbed()
-.setTitle('How to use the bot')
-.setDescription('**First you must use the Prefix which is ? \n\n second thing you must write the name of the monster like ?Voodoo like the photo :point_down: \n\n to know all monsters type ?e **')
+.setTitle('كيفيه تشغيل بوت الصيد')
+.setDescription('** اول شئ العلامه الي تشغل البوت هي ? لازم تستخدمها قبل اسم اي وحش \n\n ثاني شي عشان تعرف الوحوش الي موجوده فالبوت اكتب ?a \n\n ثالث شي اعمل نفس الي بالصوره**')
 .setTimestamp()
 .setFooter(`Requsted by ${message.author.username}`)
 .setColor(0xe69e26)
-.setImage('https://g.top4top.io/p_1649jq84y1.png')
+.setImage('https://c.top4top.io/p_1649bfxby1.png')
 message.author.send(embed);
 const embed2 = new Discord.MessageEmbed()
 .setDescription('من فضلك شوف الخاص \n\n اذا لم تتلقي اي رساله قم بتفعيل هذا الخيار')
