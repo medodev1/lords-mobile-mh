006f3ab2d6834677c7bafc6ff425b4f1376ac296
medodev1 <64170763+medodev1@users.noreply.github.com>
2020-07-07 17:10:21 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const botconfig = require("../botsettings.json");
 module.exports.run = async (bot, message, args) => {
 const embed = new Discord.MessageEmbed()
 .setTitle('please chose your language \n\n من فضلك اختار لغتك')
-.setDescription('`For English type ?EngHelp \n ?ArbHelp للعربيه اكتب`\n\n **There is ping command try it by ?ping** \n\n **يوجد امر لحساب البنق الخاص بك جربه من خلال ping?**')
+.setDescription('`For English type ?EngHelp \n ?ArbHelp للعربيه اكتب`\n\n **There is ping command try it by ?ping** \n\n **يوجد امر لحساب البنق الخاص بك جربه من خلال ping? \n\n لمعرفه كيفيه تشغيل البوت باللغه العربيه اكتب ?HTUA \n\n to know how to use the bot type ?HTUE**')
 .setColor(0xe69e26)
 .setThumbnail(message.author.displayAvatarURL())
 .setTimestamp()
